1c315a93e103b27cdc67c9e1fb6235ff2f8fde40
Jude <judo4christ@gmail.com>
2023-06-23 20:49:31 +0000

    create file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet manifest to create a file at /tmp/school with specific attributes
+
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
